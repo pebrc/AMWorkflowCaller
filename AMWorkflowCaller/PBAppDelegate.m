331f32fd97109fb35db5be605dafc0218fed0c1a
Peter Brachwitz <peter.brachwitz@gmail.com>
2012-08-27 21:57:05 +0200

    Minimal Example

--- AMWorkflowCaller/PBAppDelegate.m
+++ AMWorkflowCaller/PBAppDelegate.m
@@ -7,12 +7,30 @@
 //
 
 #import "PBAppDelegate.h"
+#import "Automator/AMWorkflow.h"
 
 @implementation PBAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+
+    NSOpenPanel * panel = [NSOpenPanel openPanel];
+	[panel setAllowsMultipleSelection:NO];
+	[panel setCanChooseFiles:YES];
+	[panel setCanChooseDirectories:NO];
+    [panel setAllowedFileTypes:[NSArray arrayWithObject: @"com.apple.automator-workflow"]];
+	NSInteger result = [panel runModal];
+	if (result == NSFileHandlingPanelOKButton) {
+        NSURL * workflow = [[panel URLs]objectAtIndex:0];
+        NSLog(@"selected url %@", workflow);
+        NSError * error = nil;
+        [AMWorkflow runWorkflowAtURL:workflow withInput:[NSArray arrayWithObject:workflow] error:&error];
+        if(error) {
+            NSLog(@"Error while executing workflow %@", [error localizedDescription]);
+        }
+        
+	}
+
 }
 
 @end
